9aef014e288dfab1ad1b198d19de563a9dc58e41
Saad Takhir <saadtakhir1@gmail.com>
2025-09-18 02:15:34 +0500

    fix upload error

--- api/audio.js
+++ api/audio.js
@@ -1,4 +1,4 @@
-export const config = { runtime: 'edge' };
+export const config = { runtime: 'nodejs' };
 
 const ENDPOINTS = {
   m4a: 'https://api.narakeet.com/text-to-speech/m4a',
--- api/audioText.js
+++ api/audioText.js
@@ -1,4 +1,4 @@
-export const config = { runtime: 'edge' };
+export const config = { runtime: 'nodejs' };
 
 export default async function handler(req) {
   const baseCorsHeaders = {
